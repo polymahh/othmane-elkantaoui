5598621739a7b0d47725e0c898b50f6e3237206a
polymahh <otman.elkantaoui@gmail.com>
2022-08-10 11:21:51 +0100

    fixed

--- src/Components/Projects/projects.jsx
+++ src/Components/Projects/projects.jsx
@@ -1,3 +1,18 @@
+import { toHaveDescription } from "@testing-library/jest-dom/dist/matchers"
+import { IoLogoVimeo } from "react-icons/io5"
+import { ProjectCard } from "../ProjectCard/projectCard"
+
+const prjectList =[
+    {
+        image:'',
+        title:'',
+        description:'',
+        stack:'',
+        liveLink:'',
+        gitLink:'',
+    }
+]
+
 export const Projects = ()=> {
     return (
     <>
@@ -5,7 +20,7 @@ export const Projects = ()=> {
         <span className="text-4xl font-bold ">PROJECTS</span>
         <div className="m-4">
         <span className="text-2xl font-semibold">BASIC</span>
-
+            <ProjectCard/>
         </div>
     </div>
     </>
