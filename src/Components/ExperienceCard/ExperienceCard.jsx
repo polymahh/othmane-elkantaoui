cf91a63d3e40630e7334f1a4f2ae1d78f58c7881
polymahh <otman.elkantaoui@gmail.com>
2022-12-08 23:38:21 +0100

    seocomponent experience

--- src/Components/ExperienceCard/ExperienceCard.jsx
+++ src/Components/ExperienceCard/ExperienceCard.jsx
@@ -4,45 +4,75 @@ import { useInView } from "react-intersection-observer";
 const ExperienceCard = ({ item }) => {
   const { ref, inView } = useInView({
     rootMargin: "-100px 0px",
+    triggerOnce: true,
   });
 
   return (
-    <div className="flex flex-col gap-4 relative  gap-6 lg:w-[976px] my-40 ">
-      <span className="text-4xl font-bold">{item.title}</span>
-      <div className=" min-w-1/2">
-        <p className="text-xl font-bold">{item.description}</p>
+    <div className=" grid  md:grid-cols-2 gap-8 relative lg:w-[976px] my-40 px-4 md:px-12 ">
+      <div className="flex flex-col  gap-8">
+        <span className="text-4xl font-bold">{item.title}</span>
+
+        <div>
+          <p className="text-lg font-bold  ">{item.description}</p>
+        </div>
+
+        <div className="relative">
+          <span ref={ref} className={`text-2xl font-bold ez translate-y-30`}>
+            What i did:
+          </span>
+          <ul className={` text-lg font-bold   `}>
+            {item.whatiDid.map((text, idx) => (
+              <>
+                <span
+                  className={`relative block  py-4 pl-8 delay-${
+                    100 * idx
+                  } ease-in  duration-500 ${
+                    inView ? "translate-y-0 " : "translate-y-36 opacity-0"
+                  } `}
+                >
+                  {" "}
+                  <IoCheckmarkCircle className="absolute top-5 left-0 text-red-p" />
+                  {text}
+                </span>
+              </>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className=" flex gap-2 px-4 ">
-        {/* image and bg box */}
+
+      <div className=" flex flex-wrap place-self-center  gap-2 sm:w-3/4 md:w-full">
+        <span className="text-md text-red-p ">{item.status}</span>
+
+        {/* images */}
         <div>
-          <img className=" object-contain " src={item.image[0]} alt="" />
+          <img
+            className={`mg: max-h-[50vh] delay-100 ease-in  duration-500 ${
+              inView ? "translate-x-0 " : "translate-x-36 opacity-0"
+            }  `}
+            src={item.image[0]}
+            alt=""
+          />
         </div>
-        <div>
-          <img className={"object-contain  "} src={item.image[1]} alt="" />
+        <div className="flex gap-2">
+          <div>
+            <img
+              className={`min-w-2/3 delay-100 ease-in  duration-500 ${
+                inView ? "translate-x-0 " : "translate-x-36 opacity-0"
+              } `}
+              src={item.image[1]}
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className={`min-w-1/3 delay-300 ease-in  duration-500 ${
+                inView ? "translate-x-0 " : "translate-x-36 opacity-0"
+              } `}
+              src={item.image[2]}
+              alt=""
+            />
+          </div>
         </div>
-        <div>
-          <img className={"object-contain "} src={item.image[2]} alt="" />
-        </div>
-      </div>
-      <div className="relative">
-        <span ref={ref} className={`text-2xl font-bold ez translate-y-30`}>
-          What i did:
-        </span>
-        <ul
-          className={` text-xl font-bold left-0  ez ${
-            inView ? "translate-y-2 " : "-translate-y-90 opacity-0"
-          } `}
-        >
-          {item.whatiDid.map((text) => (
-            <>
-              <span className=" relative block py-4 pl-8">
-                {" "}
-                <IoCheckmarkCircle className="absolute top-5 left-0 text-red-p" />
-                {text}
-              </span>
-            </>
-          ))}
-        </ul>
       </div>
     </div>
   );
--- src/Components/Experiences/Experiences.jsx
+++ src/Components/Experiences/Experiences.jsx
@@ -2,20 +2,24 @@ import { IoSnow } from "react-icons/io5";
 import ExperienceCard from "../ExperienceCard/ExperienceCard";
 
 const experienceList = [
-  // {
-  //   image: "rest-countries-api.jpg",
-  //   title: "seocomponent.com",
-  //   description:
-  //     "seocomponent.com is a collection of all types oftechnical seo elements for websites so developers sped more time designing and building ",
-  //   whatiDid: [
-  //     "Built new schema generators for various types of pages e.g.: Article,Event, Recipe , Job Posting …",
-  //     "Optimized the code by creating new reusable components and tools so to make creation of new generators easier",
-  //     "Establishing a new context to share states between components seamlessly",
-  //   ],
-  //   stack: ["Nextjs", "Typescript", "TailwindCss", "Context api"],
-  //   status: "Freelance (2022-09/2022-11)",
-  //   liveLink: "https://www.seocomponent.com/",
-  // },
+  {
+    image: [
+      "seocomponent-desktop.png",
+      "seocomponent-tablet.png",
+      "seocomponent-mobile.png",
+    ],
+    title: "seocomponent.com",
+    description:
+      "seocomponent.com is a collection of all types of technical SEO elements for websites so developers spend more time designing and building websites ",
+    whatiDid: [
+      "Built new schema generators for various types of pages e.g.: Article,Event, Recipe , Job Posting and many more",
+      "Optimized the code by creating new reusable components and tools so to make creation of new generators easier",
+      "Establishing a new context to share states between components seamlessly",
+    ],
+    stack: ["Nextjs", "Typescript", "TailwindCss", "Context api"],
+    status: "Freelance (2022-09/2022-11)",
+    liveLink: "https://www.seocomponent.com/",
+  },
   {
     image: [
       "datamine-desktop.png",
@@ -24,14 +28,14 @@ const experienceList = [
     ],
     title: "Data Mine",
     description:
-      "Data mine is an open source project to create a public facing directory website for Prifina dynamic data to easily find all types of data objects and attributes available for the development of user-held data apps.",
+      "Data mine is an open-source project to create a public facing directory website for Prifina dynamic data to easily find all types of data objects and attributes available for developers to create all kinds data apps.",
     whatiDid: [
-      "Worked with a ui/ux designer to make the frontend of theproject following figma designs",
-      "Connected the website to Prifina notion data-base through an express server and established the routing and a context to make calls to the api only if needed ",
+      "Worked with a ui/ux designer to make the frontend of the project following figma designs",
+      "Connected the website to Prifina notion data-base through an express server and established the routing and a context to make calls to the api as needed ",
       "Coded the sorting by search or by categories or by status logic from scratch",
     ],
     stack: ["Nextjs", "Typescript", "TailwindCss", "Context api"],
-    status: "OpenSource (in Progress 2022-11 / present)",
+    status: "OpenSource (2022-11 / present) in progress ...",
     liveLink: "http://data-mine.vercel.app/",
   },
 ];
