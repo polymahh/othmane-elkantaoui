48806a9f9a425653214720fec4b5bc1a7c695c43
polymahh <otman.elkantaoui@gmail.com>
2023-07-09 20:55:41 +0100

    smaller profile pic webp

--- src/Components/About/about.jsx
+++ src/Components/About/about.jsx
@@ -16,14 +16,14 @@ export const About = (props) => {
               : "opacity-0 -translate-x-1/3"
           }
         >
-          <div className="text-4xl   font-bold">
+          <div className="text-4xl font-bold">
             HI, I'M OTHMANE.
             <br />
             A FRONT-END DEVELOPER <br />
             WITH A PASSION FOR <br />
             ALL THINGS WEB DEVELOPMENT.
           </div>
-          <div className="mt-4 text-xl font-semibold  items-center gap-2 sm:flex ">
+          <div className="items-center gap-2 mt-4 text-xl font-semibold sm:flex ">
             <span>FRONT-END </span>
             <IoSnow className="text-sm text-red-p" />
             <span> DEVELOPMENT </span>
@@ -34,7 +34,7 @@ export const About = (props) => {
           <div className="flex justify-center">
             {/* <button className="mr-0 md:mr-8 bg-black-p text-grey-p hover:bg-grey-p hover:text-black-p">DOWLOAD RESUME</button> */}
             <a
-              className="btn mr-0 md:mr-8 bg-black-p text-grey-p hover:bg-grey-p hover:text-black-p"
+              className="mr-0 btn md:mr-8 bg-black-p text-grey-p hover:bg-grey-p hover:text-black-p"
               href="othmane-elkantaoui-cv.pdf"
               download
             >
@@ -58,82 +58,82 @@ export const About = (props) => {
               ? "opacity-100 ease-in delay-200 duration-500 sm:w-3/4 m-auto"
               : "opacity-0 translate-x-1/3"
           }
-          src="profile4.png"
+          src="profile4.webp"
           alt="othmane"
         />
       </div>
       {/* scrolling bar */}
-      <div className="relative overflow-hidden min-w-full bg-black-p p-12 my-6 whitespace-nowrap">
-        <div className="absolute top-0 left-0 bg-black-p text-grey-p text-2xl flex items-center justify-start gap-6 transition-all delay-100 ease-linear h-full">
+      <div className="relative min-w-full p-12 my-6 overflow-hidden bg-black-p whitespace-nowrap">
+        <div className="absolute top-0 left-0 flex items-center justify-start h-full gap-6 text-2xl transition-all ease-linear delay-100 bg-black-p text-grey-p">
           <div className="flex anim">
             <div
               id="holder"
-              className="transition-all delay-200 ease-linear flex items-center"
+              className="flex items-center transition-all ease-linear delay-200"
             >
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>React.js</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>JavaScript</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>HTML5</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>CSS3</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>TailWindCss</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Jest</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Webpack</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>NPM</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Next.js</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Redux</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>TypeScript</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Chakra UI</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>jQuery</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Bootstrap</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Git</h4>
             </div>
             <div
               id="holder"
-              className="transition-all delay-200 ease-linear flex items-center"
+              className="flex items-center transition-all ease-linear delay-200"
             >
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>React.js</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>JavaScript</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>HTML5</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>CSS3</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>TailWindCss</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Jest</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Webpack</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>NPM</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Next.js</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Redux</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>TypeScript</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Chakra UI</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>jQuery</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Bootstrap</h4>
-              <IoSnow className="text-xl text-red-p mx-10" />
+              <IoSnow className="mx-10 text-xl text-red-p" />
               <h4>Git</h4>
             </div>
           </div>
